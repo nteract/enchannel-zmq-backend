3626a5b9b6fa9e172bddeba3b90b4185c4ac83bc
Kyle Kelley <rgbkrk@gmail.com>
2016-01-12 00:21:37 -0600

    Helper payload run.

--- scripts/repl.js
+++ scripts/repl.js
@@ -1,4 +1,9 @@
+/* eslint camelcase: 0 */
+//                   ^^---- for ignoring Jupyter JSON, like msg_id
+
 global.enchannelZMQ = require('../src');
+const uuid = require('uuid');
+global.uuid = uuid;
 
 (() => {
   const chalk = require('chalk');
@@ -12,6 +17,29 @@ global.enchannelZMQ = require('../src');
   console.log(insp(global.enchannelZMQ));
 
   console.log(chalk.green('\nHINT: ') + chalk.white('var enchan = enchannelZMQ(require(\'./kernel.json\'))\n'));
+
+  console.log('Example payload: ');
+
+  const message = {
+    header: {
+      msg_id: `execute_${uuid.v4()}`,
+      username: '',
+      session: '00000000-0000-0000-0000-000000000000',
+      msg_type: 'execute_request',
+      version: '5.0',
+    },
+    content: {
+      code: 'print("woo")',
+      silent: false,
+      store_history: true,
+      user_expressions: {},
+      allow_stdin: false,
+    },
+  };
+
+  console.log('var payload = ');
+  console.log(message);
+
 })();
 
 require('repl').start('> ');
