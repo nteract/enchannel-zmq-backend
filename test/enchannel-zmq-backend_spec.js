2e4d94b4175e48425441fc6f384e4d589a0e9d3f
Kyle Kelley <rgbkrk@gmail.com>
2016-01-17 14:20:24 -0600

    Expand on what gets tested in this test.

--- test/enchannel-zmq-backend_spec.js
+++ test/enchannel-zmq-backend_spec.js
@@ -2,9 +2,10 @@
 
 import {
   createIOPubSubject,
+  createShellSubject,
 } from '../src';
 
-import { Subject } from 'rx';
+import { AnonymousSubject } from 'rx';
 
 import * as jmp from 'jmp';
 import { getPort } from 'portfinder';
@@ -15,8 +16,8 @@ describe('createIOPubSubject', () => {
   it('returns an Rx.Subject', (done) => {
     const scheme = 'sha256';
     const key = '5ca1ab1e-c0da-aced-cafe-c0ffeefacade';
-
     const pub = new jmp.Socket('pub', scheme, key);
+
     getPort((err, port) => {
       if(err) {
         throw err;
@@ -31,8 +32,60 @@ describe('createIOPubSubject', () => {
           iopub_port: port,
         };
         const s = createIOPubSubject(config);
-        expect(s).to.be.an.instanceof(Subject);
+        expect(s).to.be.an.instanceof(AnonymousSubject);
+        s.close();
         done();
+      });
+    });
+  });
+});
+
+describe('createShellSubject', () => {
+  it('allows for sending messages on the shell channel', (done) => {
+    const scheme = 'sha256';
+    const key = '5ca1ab1e-c0da-aced-cafe-c0ffeefacade';
+    const kernel = new jmp.Socket('router', scheme, key);
+
+    getPort((err, port) => {
+      if(err) {
+        throw err;
+      }
+      const address = `tcp://127.0.0.1:${port}`;
+
+      kernel.bind(address, () => {
+        const config = {
+          ip: '127.0.0.1',
+          transport: 'tcp',
+          signature_scheme: 'hmac-sha256',
+          shell_port: port,
+        };
+        const s = createShellSubject(config);
+        expect(s).to.be.an.instanceof(AnonymousSubject);
+
+        kernel.on('message', (msg) => {
+          console.log(msg);
+          msg.respond(kernel, 'kernel_info_reply', {}, {});
+        });
+
+        s.subscribe(x => {
+          expect(x).to.equal(2);
+          s.close();
+          done();
+        });
+
+        s.send({
+          header: {
+            msg_id: '00000',
+            username: 'faker',
+            session: '00000',
+            msg_type: 'kernel_info_request',
+            version: '5.0',
+          },
+          parent_header: {},
+          metadata: {},
+          content: {},
+        });
+
       });
     });
   });
