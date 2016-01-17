5678ab57b86f2e3a0b70dc016c7d185a46083f24
Kyle Kelley <rgbkrk@gmail.com>
2016-01-17 14:54:36 -0600

    Test the created observers

--- test/subjection_spec.js
+++ test/subjection_spec.js
@@ -1,7 +1,14 @@
-import { expect } from 'chai';
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+chai.use(sinonChai);
+
+import * as jmp from 'jmp';
 
 import {
   deepFreeze,
+  createObserver,
 } from '../src/subjection';
 
 describe('deepFreeze', () => {
@@ -20,5 +27,32 @@ describe('deepFreeze', () => {
     expect((() => {obj.a = 2;})).to.throw('Cannot assign to read only property');
     expect((() => {obj.b.bb.c = 42;})).to.throw('Cannot assign to read only property');
     expect((() => {obj.d = 12;})).to.throw('Can\'t add property d, object is not extensible');
+  });
+});
+
+describe('createObserver', () => {
+  it('creates an observer from a socket', () => {
+    const hokeySocket = {
+      send: sinon.spy(),
+      removeAllListeners: sinon.spy(),
+      close: sinon.spy(),
+    };
+
+    const ob = createObserver(hokeySocket);
+    const message = { content: { x: 2 } };
+    ob.onNext(message);
+    expect(hokeySocket.send).to.have.been.calledWith(new jmp.Message(message));
+  });
+  it('removes all listeneres and closes the socket onCompleted', () => {
+    const hokeySocket = {
+      send: sinon.spy(),
+      removeAllListeners: sinon.spy(),
+      close: sinon.spy(),
+    };
+
+    const ob = createObserver(hokeySocket);
+    ob.onCompleted();
+    expect(hokeySocket.removeAllListeners).to.have.been.calledWith();
+    expect(hokeySocket.close).to.have.been.calledWith();
   });
 });
