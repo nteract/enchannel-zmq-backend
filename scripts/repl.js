a88b3683cc07658fbb40ce80880f92f518b08357
Kyle Kelley <rgbkrk@gmail.com>
2016-01-23 13:38:14 -0600

    Autodetect a kernel for playing with subscriptions

--- scripts/repl.js
+++ scripts/repl.js
@@ -1,52 +1,61 @@
 /* eslint camelcase: 0 */
 //                   ^^---- for ignoring Jupyter JSON, like msg_id
 
-global.enchannelZMQ = require('../src');
-global.createShellSubject = enchannelZMQ.createShellSubject;
-global.createIOPubSubject = enchannelZMQ.createIOPubSubject;
+const enchannelZMQ = require('../src');
+const createShellSubject = enchannelZMQ.createShellSubject;
+const createIOPubSubject = enchannelZMQ.createIOPubSubject;
 const uuid = require('uuid');
-global.uuid = uuid;
+const path = require('path');
 
-(() => {
-  const chalk = require('chalk');
-  const util = require('util');
+const chalk = require('chalk');
+const identity = uuid.v4();
 
-  // Quick helper functions
-  const codeLog = (s) => console.log(chalk.white(chalk.bgBlack(s)));
-  const insp = o => util.inspect(o, { colors: true });
+const fsp = require('fs-promise');
+const runtimeDir = require('jupyter-paths').runtimeDir();
 
-  codeLog('\nconsole.log(enchannelZMQ)');
-  console.log(insp(global.enchannelZMQ));
+fsp.readdir(runtimeDir)
+   .then(data => {
+	  const kernels = data.filter(x => /kernel.+\.json/.test(x))
+	  if(kernels.length <= 0) throw new Error('You need a running Jupyter session');
+    console.log('Using the first kernel!');
+    return path.join(runtimeDir, kernels[0]);
+	})
+  .then(runtimePath => {
+    return fsp.readFile(runtimePath);
+  })
+  .then(contents => {
+    return JSON.parse(contents);
+  })
+  .then(kernelConfig => {
+  	const shell = createShellSubject(identity, kernelConfig);
+    const iopub = createIOPubSubject(identity, kernelConfig);
+    global.shell = shell;
+    global.iopub = iopub;
 
-  codeLog('\nvar identity = uuid.v4()');
-  global.identity = uuid.v4();
+    global.message = {
+      header: {
+        msg_id: `execute_${uuid.v4()}`,
+        username: '',
+        session: '00000000-0000-0000-0000-000000000000',
+        msg_type: 'execute_request',
+        version: '5.0',
+      },
+      content: {
+        code: 'print("woo")',
+        silent: false,
+        store_history: true,
+        user_expressions: {},
+        allow_stdin: false,
+      },
+    };
+  
+    console.log(chalk.green('\nHINT: ') + chalk.white('iopub.subscribe(console.log, console.error)'))
+    console.log(chalk.green('\nHINT: ') + chalk.white('shell.subscribe(console.log, console.error)'))
+    console.log(chalk.green('\nHINT: ') + chalk.white('shell.send(message)'))
 
-  console.log(chalk.green('\nHINT: ') + chalk.white('var config = require(\'./kernel.json\')'));
-  console.log(chalk.white('var shell = createShellSubject(identity, config)'));
+	  require('repl').start('> ');
+  })
+  .catch(err => {
+    console.error(err);
+  })
 
-  const message = {
-    header: {
-      msg_id: `execute_${uuid.v4()}`,
-      username: '',
-      session: '00000000-0000-0000-0000-000000000000',
-      msg_type: 'execute_request',
-      version: '5.0',
-    },
-    content: {
-      code: 'print("woo")',
-      silent: false,
-      store_history: true,
-      user_expressions: {},
-      allow_stdin: false,
-    },
-  };
-
-  console.log('var payload = ');
-  console.log(message);
-  console.log('\n      ' + chalk.white('iopub.subscribe(console.log)'));
-  console.log('\n      ' + chalk.white('shell.subscribe(console.log)'));
-  console.log('\n      ' + chalk.white('shell.send(payload)'));
-
-})();
-
-require('repl').start('> ');
